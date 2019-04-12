e705f1a295c1821de5dd6d6a335a4ba10db1198e
Margal <mgalloperalta@gmail.com>
2019-04-12 16:46:59 +0200

    Adjust user model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, default: '', required: true },
+  age: { type: Number, default: '' },
+  gender: { type: String, default: '' },
+  description: { type: String, default: '' },
+  userImage: { type: String },
 }, {
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
-  },
-});
+   },
+  });
 
 const User = mongoose.model('User', userSchema);
 
