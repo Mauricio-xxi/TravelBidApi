ba23a3aa878ae1907cdd8d93b61f687e98ab3084
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-04-16 23:44:10 +0200

    add mutation on graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,16 @@
 export const resolvers = {
-
-  
-}
+  Query :{
+    hello: ()=>{
+      return "hola con graphql"
+    },
+  User: ()=> {
+      return null
+    }
+  },
+  Mutation: {
+    createUser(_,{input}){
+      console.log(input)
+      return null 
+    }
+  }
+};
--- graphql/schema.js
+++ graphql/schema.js
@@ -2,9 +2,22 @@ import {makeExecutableSchema} from "graphql-tools"
 import {resolvers} from "./resolvers"
 const typeDefs = `
   type Query {
-    hello: String!
+    hello: String
+    User: User
+  },
+  type User {
+    email: String!,
+    password: String!,
   }
 
+  type Mutation {
+    createUser(input: UserInput): User
+  }
+
+  input UserInput{
+    email: String!,
+    password: String!,
+  }
 `;
 
 export default makeExecutableSchema({
