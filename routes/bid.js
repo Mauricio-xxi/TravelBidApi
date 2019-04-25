376a54ac18bfd1a8dc07d498debbfac6bbf1b531
Margal <mgalloperalta@gmail.com>
2019-04-25 20:49:21 +0200

    Delete unused routes

--- routes/bid.js
+++ routes/bid.js
@@ -57,11 +57,12 @@ router.delete('/:bidID', (req, res, next) => {
 
 
 router.put('/:bidID', isLoggedIn(), (req, res, next) => {
-  const { description, value } = req.body;
+  const { description, value, Status } = req.body;
   const bidID = req.params.bidID;
   Bid.findByIdAndUpdate(bidID, {
     description,
     value,
+    Status,
   }, { new: true })
     .then((bid) => {
       res.json({bid});
@@ -70,58 +71,5 @@ router.put('/:bidID', isLoggedIn(), (req, res, next) => {
       next(error);
     });
 });
-
-// router.get('/:id', (req, res, next) => {
-//   const offerID = req.params.id;
-
-//   Offer.findById(offerID)
-//     .then((offer) => {
-//       res.status(200);
-//       res.json(offer);
-//     })
-//     .catch(next);
-
-// });
-
-
-// // GET SEARCH INPUT
-// router.get('/search/:city', (req, res, next) => {
-//   const { city } = req.params;
-//   Offer.find({ location: city })
-//     .then((offers) => {
-//       res.status(200);
-//       res.json(offers);
-//     })
-//     .catch(next);
-// });
-
-// router.put('/:id', isLoggedIn(), (req, res, next) => {
-//   const { from, until, budget } = req.body;
-//   const offerID = req.params.id;
-//   // conbid = req.session.currentUser._id;
-//   Offer.findByIdAndUpdate(offerID, {
-//     // userID,
-//     from,
-//     until,
-//     budget,
-//   }, { new: true })
-//     .then((offer) => {
-//       res.json({offer});
-//     })
-//     .catch((error) => {
-//       next(error);
-//     });
-// });
-
-// router.delete('/:id', isLoggedIn(), (req, res, next) => {
-//   const offerID = req.params.id;
-//   Offer.findByIdAndDelete(offerID)
-//     .then(() => {
-//       res.json({ message: `Offer with ${offerID} is removed successfully.` });
-//     })
-//     .catch((error) => {
-//       next(error);
-//     });
-// });
 
 module.exports = router;
